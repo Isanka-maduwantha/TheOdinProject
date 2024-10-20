42a8573c03c2994f7b1c96508bbbb70b84a6219c
kamal738 <maduwanthaisanka@gmail.com>
2024-10-20 15:07:50 +0530

    Make log Winner or loser

--- 02RockPaperScissors/script.js
+++ 02RockPaperScissors/script.js
@@ -3,41 +3,97 @@ let computerScore = 0;
 
 function getComputerChoice(){
     let randomMove = (Math.random()*9)
-    // console.log(randomMove); 
+    
+
+    if (randomMove>= 0 && randomMove< 3){
+        randomMove= "Rock";
+    }
+    else if (randomMove>= 3 && randomMove< 6){
+        randomMove= "Paper";
+    }
+    else{
+        randomMove="Scissors";
+    }
+    console.log("ME: "+randomMove); 
+    return randomMove;
+
 }
 
 function getUserChoice(){
     let userInput = prompt("Your Move Rock/Paper/Scissors: ");
-    console.log(userInput);
+    
+    
+    userInput = userInput.toLowerCase();
+    // console.log(userInput);
+    
+    if (userInput == 'r' | userInput =='rock'){
+        userInput = 'Rock';
+
+    }
+    else if (userInput == 'p' | userInput =='paper'){
+        userInput = 'Paper';
+    }
+    else {
+        userInput = 'Scissors';
+    }
+ console.log("YOU: "+userInput);
+  
     return userInput;
 }
 
-function playRound(userChoice,computerChoice){
-    console.log(userChoice);
-   let choice = userChoice.toLowerCase();
+function playRound(userMove,computerMove){
+    let loose = "You Lose";
+    let win = "You Win";
+    let draw = `Its a Draw You Entered "${userMove}" I entered "${userMove}"`;
+   if (userMove == 'Rock'){
+        if(computerMove== 'Paper'){
+            console.log(loose);
+        }
+        else if(computerMove =="Scissors"){
+            console.log(win);
+        }
+        else {
+            console.log(draw);
+        }
+   }
+   else if (userMove == 'Paper'){
+        if(computerMove== 'Rock'){
+            console.log(win);
+        }
+        else if(computerMove =="Scissors"){
+            console.log(loose);
+        }
+        else {
+            console.log(draw);
+    }
 
-    if (userChoice == 'r' | userChoice =='rock'){
-        userChoice = 'Rock';
+   }else if (userMove == 'Scissors'){
+        if(computerMove== 'Paper'){
+            console.log(win);
+        }
+        else if(computerMove =="Rock"){
+            console.log(loose);
+        }
+        else {
+            console.log(draw);
+        }
 
-    }
-    else if (userChoice == 'p' | userChoice =='paper'){
-        userChoice = 'Paper';
-    }
-    else {
-        userChoice = 'Scissors';
-    }
+   
 
-    if (computerChoice >= 0 && computerChoice < 3){
-        computerChoice = "Rock";
-    }
-    else if (computerChoice >= 3 && computerChoice < 6){
-        computerChoice = "Paper";
+    console.log(userMove,computerMove);
+ 
+    
+}}
+playRound(getUserChoice(),getComputerChoice());
+for(let i = 1; i < 2; ){
+   let reMatch = prompt("Do you want to play Again? y/n ");
+    reMatch = reMatch.toLocaleLowerCase();
+    if (reMatch == "y"| reMatch =="yes"){
+        playRound(getUserChoice(),getComputerChoice());
     }
     else{
-        computerChoice ="Scissors";
+        i = 2;
     }
-    console.log(userChoice,computerChoice);
 
-    
+ 
 }
-playRound(getUserChoice(),getComputerChoice());
